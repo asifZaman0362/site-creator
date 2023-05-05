e20934bb9671077290f513e2cbdfeed222b9434a
asifZaman0362 <zero362001@gmail.com>
2023-05-06 01:14:04 +0530

    Have some basic drag and drop working

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 let templateList = null;
+let target = null;
+let projectRoot = null;
 
 function addElement(element, root) {
   root.appendChild(element);
 }
 
 function hoverItem(item) {
-  console.debug(item);
-  if (templateList)
+  if (templateList) {
     templateList.classList.add("shown");
+    if (target) target.classList.remove("active");
+    target = document.querySelector(`#${item}`);
+    if (target) target.classList.add("active");
+  }
 }
 
 function stopHover() {
@@ -19,8 +24,40 @@ function createInterface() {
 
 }
 
+function dragOver(ev) {
+  ev.preventDefault();
+  ev.dataTransfer.dropEffect = "copy";
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  const data = ev.dataTransfer.getData("text/html");
+  let wrapperNode = document.createElement("div");
+  wrapperNode.innerHTML = data;
+  let createdElement = wrapperNode.firstChild;
+  if (projectRoot) {
+    projectRoot.appendChild(createdElement);
+  }
+}
+
+function startDrag(ev) {
+  ev.dataTransfer.effectAllowed = "copy";
+  let element = ev.target.querySelector(".module");
+  ev.dataTransfer.setData("text/html", element.outerHTML);
+}
+
 function setup() {
   templateList = document.querySelector("#template-list");
+  let elements = document.getElementsByClassName("template-item-wrapper");
+  for (let element of elements) {
+    element.draggable = true;
+    element.ondragstart = startDrag;
+  }
+  projectRoot = document.querySelector("#project-root");
+  if (projectRoot) {
+    projectRoot.ondragover = dragOver;
+    projectRoot.ondrop = drop;
+  }
 }
 
 window.addEventListener("load", setup);
