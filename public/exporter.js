6280c8744a0260d0a034f1c3a9ba0aba5b327d7d
asifZaman0362 <zero362001@gmail.com>
2023-06-17 15:14:14 +0530

    html sucks balls

--- public/exporter.js
+++ public/exporter.js
@@ -1,11 +1,53 @@
-function recursiveInline(item) {
+function addFonts() {
+  let links = [];
+  for (const styleSheet of document.styleSheets) {
+    if (styleSheet.href != null && !styleSheet.href.match('style.css')) {
+      let link = document.createElement('link');
+      link.rel = 'stylesheet';
+      link.href = styleSheet.href;
+      links.push(link.outerHTML);
+    }
+  }
+  if (links.length == 0) return '';
+  let linkText = '';
+  for (const link of links) {
+    linkText += '\n' + link;
+  }
+  return linkText;
+}
 
+function removeEditableRecursive(element) {
+  element.contentEditable = false;
+  element.draggable = false;
+  for (let i = 0; i < element.children.length; i++) {
+    let child = element.children[i];
+    removeEditableRecursive(child);
+  }
+}
+
+function getStylesRecursive(element, styles) {
+  let rules = [];
+  let keys = Object.keys(element.style);
+  let n = keys.findIndex(key => isNaN(parseInt(key)));
+  let availableStyles = keys.slice(0, n);
+  for (const style in availableStyles) {
+    const styleName = element.style[style];
+    let value = element.style.getPropertyValue(styleName);
+    rules.push({ name: styleName, value: value });
+  }
+  styles.set(element, rules);
+  for (let i = 0; i < element.children.length; i++) {
+    getStylesRecursive(element.children[i], styles);
+  }
 }
 
 function exportHtml(workspace, urls) {
   const copiedContent = document.createElement('div');
   copiedContent.innerHTML = workspace.outerHTML;
   copiedContent.querySelector('#placeholder').remove();
+  removeEditableRecursive(copiedContent);
+  let modifiedStyles = new Map();
+  getStylesRecursive(workspace, modifiedStyles);
   urls.forEach(url => {
     if (url.enabled && url.value) {
       let element = copiedContent.querySelector(`[data-url-id="${url.element.dataset.urlId}"`);
@@ -31,30 +73,34 @@ function exportHtml(workspace, urls) {
   });
   const styles = document.styleSheets;
   for (const css of styles) {
-    const rules = css.cssRules || StyleSheet.rules;
-
-    for (const rule of rules) {
-      if (!rule.selectorText) continue;
-      let keys = Object.keys(rule.style);
-      let n = keys.findIndex(key => isNaN(parseInt(key)));
-      let availableStyles = keys.slice(0, n);
-      const selectors = rule.selectorText.split(',');
-      for (const selector of selectors) {
-        const elements = copiedContent.querySelectorAll(selector);
-        for (const element of elements) {
-          console.debug(selector, element);
-          for (const key of availableStyles) {
-            const styleName = rule.style[key];
-            //console.debug(styleName);
-            let value = rule.style.getPropertyValue(styleName);
-            //console.log(styleName, ':', value);
-            element.style.setProperty(styleName, value);
+    try {
+      const rules = css.cssRules || StyleSheet.rules;
+      for (const rule of rules) {
+        if (!rule.selectorText) continue;
+        let keys = Object.keys(rule.style);
+        let n = keys.findIndex(key => isNaN(parseInt(key)));
+        let availableStyles = keys.slice(0, n);
+        const selectors = rule.selectorText.split(',');
+        for (const selector of selectors) {
+          const elements = copiedContent.querySelectorAll(selector);
+          for (const element of elements) {
+            for (const key of availableStyles) {
+              const styleName = rule.style[key];
+              let value = rule.style.getPropertyValue(styleName);
+              //element.style.setProperty(styleName, value);
+            }
           }
-          //console.log(`Style used for selector '${selector}': '${rule.style}'`);
         }
       }
+    } catch (_err) {
+      continue;
     }
   }
+  modifiedStyles.forEach((rules, element) => {
+    rules.forEach(rule => {
+      element.style.setProperty(rule.name, rule.value);
+    });
+  });
   //recursiveInline(copiedContent);
   let html = `
     <!DOCTYPE html>
@@ -63,7 +109,7 @@ function exportHtml(workspace, urls) {
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>Document</title>
-      <link rel="stylesheet" href="style.css">
+      ${addFonts()}
     </head>
     <body>
       ${copiedContent.innerHTML}
@@ -71,9 +117,9 @@ function exportHtml(workspace, urls) {
     </html>
   `;
   const exportedHtml = html;
-  console.log(copiedContent);
+  console.log(exportedHtml);
   const fileName = 'exported.html';
-  downloadHTML(exportedHtml, fileName);
+  //downloadHTML(exportedHtml, fileName);
 }
 
 function downloadHTML(htmlContent, fileName) {
